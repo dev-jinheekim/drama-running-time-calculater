8e6e02faceb7a9a4277ea702ef72c0304d608f15
Kim Jin Hee <jhkim@plus21kr.com>
2016-06-16 19:22:11 +0900

    api edit

--- app.js
+++ app.js
@@ -18,40 +18,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // routing
 app.get('/', function(req, res) {
-  res.render('index', {title:'Express'});
+  res.render('index');
 });
 
 app.get('/search', function(req, res) {
   
   // search drama title
-  var search_title = req.body.search_title; 
+  var search_title = req.param('search_title'); 
 
   // movie DB key
   var api_key = 'f073217cc9ebdffe91577b5c969ac1a3';
-  
-  var object = request({
+
+  request({
     method: 'GET',
-    url: 'http://api.themoviedb.org/3/search/tv?query=' + search_title + '&api_key=' + api_key,
+    url: 'http://api.themoviedb.org/3/search/tv?query=' + search_title + '&api_key=' + api_key, 
     headers: {
-      'Accept': 'application/json'
-    }}, function (error, response, body) {
+      'Accept':'application/json'
+    }}, function (error, response, body){
+
       if(!error && response.statusCode == 200){
-        res.send('Status:' + response.statusCode + '<br>Headers:' + JSON.stringify(response.headers));
-        console.log('Response:', body);
-        // var original = JSON.parse(body);
-        // var result = {
-        //   list: original.list,
-        //   aaaa: original.aaa
-        // };
-        // res.send(JSON.stringify(result));
-        // res.json(result);
-      }else{
-        res.send('bad request!!');
+
+       res.json(body);
+
+      } else {
+        res.send('bad request !!')
       }
-  );
+
+    });
 });
-
-
 
 app.listen(3005, function() {
   console.log('Connected 3005');
